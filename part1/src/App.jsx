cca3bd9be5cb8f25d09a4fd69f26cffa1a222433
Bruhanova Olga <bruhanovaolga940@gmail.com>
2026-07-25 14:12:11 +0700

    done ex 1.1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,15 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+const Header = (props) => {
+  return(
+  <div>
+    <h1>{props.name}</h1>
+  </div>
+  )}
 
-function App() {
-  const [count, setCount] = useState(0)
+const Content=(props)=>{
+  return(
+    <div className="content">
+      <p>{props.title} - {props.count}</p>
+    </div>
+  )}
+
+const Total=(props)=>{
+  return(
+    <div>
+      <p>Number of exersises {props.sum}</p>
+    </div>
+)}
+    
+
+const App = () => {
+  const course = 'Half Stack application development'
+
+  // const content = [
+  //   { part: 'Fundamentals of React', ex: 10 },
+  //   { part: 'Using props to pass data', ex: 7 },
+  //   { part: 'State of a component', ex: 14 },
+  // ]
+
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const totalExercises = exercises1 + exercises2+ exercises3
 
   return (
     <div>
-      <p>Hello world</p>
+      <Header name={course} />
+      {/* {content.map((item) => (
+        <Content
+          key={item.part}
+          title={item.part}
+          count={item.ex}
+        />
+      ))} */}
+      <Content title={part1} count={exercises1} />
+      <Content title={part2} count={exercises2} />
+      <Content title={part3} count={exercises3} />
+      <Total sum={totalExercises}/>
     </div>
   )
 }
